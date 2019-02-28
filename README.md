212268427143f34a5446933e61a5ea7dd37aa4d9
Phaneendra B <phani.brahma@gmail.com>
2019-02-28 00:17:09 -0600

    README Added and Prime bug fix

---
+++ README.md
@@ -0,0 +1,51 @@
+# Prime Numbers Multiplication
+
+The program prints out a multiplication table of first N primes.(Defaults to 10 if no input is given). Outputs to STDOUT
+
+## Installation
+
+Clone the repo from this Github URL
+
+```
+https://github.com/phanib/prime-table-multiplication
+```
+
+### Method 1
+
+And then execute:
+
+    $ bundle
+
+### Method 2
+
+And then execute:
+  
+ \$ bin/setup
+
+## Usage
+
+Execute this command to run the program
+
+    $ bin/start
+
+## Running the tests
+
+To run the tests, execute
+
+    $ rspec
+
+## Development
+
+After checking out the repo, run `bin/setup` to install dependencies. Program code is in `lib/` Then, run `rspec` to run the tests.
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/phanib/prime-table-multiplication. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+
+## License
+
+This program is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+
+## Code of Conduct
+
+Everyone interacting in the Prime Table Multiplication project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/[USERNAME]/test/blob/master/CODE_OF_CONDUCT.md).
--- lib/prime.rb
+++ lib/prime.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #
 # Prime library
 #
@@ -5,40 +6,40 @@
 #   Prime.is_prime?(2)           #=> true
 #
 class Prime
-    # Returns true if the input value is a prime number, else returns false.
-    #
-    # == Parameters
-    #
-    # value - Input Integer to be checked
-    def self.is_prime?(value)
-      # Sanity check for input value to be integer
-      raise ArgumentError, "Expecting an Integer, got #{value}" unless value.integer?
-  
-      # Edge cases
-      return false if value < 2
-      return true if value <= 3
-  
-      # Returns false if the value is divisible by 2 or 3
-      if (value % 2).zero? || (value % 3).zero?
-        return false
-      end
-  
-      range = (5..Math.sqrt(value) + 1).step(2).to_a
-  
-      range.each do |i|
-        # Return false if the range item divisible by input value
-        return false if (i.round % value).zero?
-      end
-  
-      # Returns true if above conditions are not met
-      true
+  # Returns true if the input value is a prime number, else returns false.
+  #
+  # == Parameters
+  #
+  # value - Input Integer to be checked
+  def self.is_prime?(value)
+    # Sanity check for input value to be integer
+    raise ArgumentError, "Expecting an Integer, got #{value}" unless value.integer?
+
+    # Edge cases
+    return false if value < 2
+    return true if value <= 3
+
+    # Returns false if the value is divisible by 2 or 3
+    if (value % 2).zero? || (value % 3).zero?
+      return false
     end
-  
-    def self.all_primes(max)
-      primes = []
-      (1..max).to_a.each do |value|
-        primes << value if is_prime?(value)
-      end
-      primes
+
+    range = (5..(Math.sqrt(value).round) + 1).step(2).to_a
+
+    range.each do |i|
+      # Return false if the range item divisible by input value
+      return false if (value % i).zero?
     end
+
+    # Returns true if above conditions are not met
+    true
   end
+
+  def self.all_primes(max)
+    primes = []
+    (1..max).to_a.each do |value|
+      primes << value if is_prime?(value)
+    end
+    primes
+  end
+end
