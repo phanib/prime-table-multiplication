3ad066544fa0cb0288077ecde9acc04454539142
Phaneendra B <phani.brahma@gmail.com>
2019-02-27 23:07:16 -0600

    Added Prime class with empty stubs

---
+++ lib/prime.rb
@@ -0,0 +1,7 @@
+class Prime
+    def self.is_prime?(value)
+    end
+  
+    def self.all_primes(max)
+    end
+end
---
+++ spec/prime_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/prime.rb'
+
+RSpec.describe Prime do
+  describe '#is_prime?' do
+  end
+
+  describe '#all_primes' do
+  end
+end
